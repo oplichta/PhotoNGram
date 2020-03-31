ab8ebde039a686c0b2a96ee50570e5568f306a47
Oskar <oskar@rescompany.com>
2020-03-31 20:48:33 +0200

    Fix for deploy

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require("express");
 
 const app = express();
 
-app.use(express.static("./dist/photongram"));
+app.use(express.static("./dist/PhotoNGram"));
 
 app.get("/*", (req, res) =>
-  res.sendFile("index.html", { root: "dist/photongram/" })
+  res.sendFile("index.html", { root: "dist/PhotoNGram/" })
 );
 
 app.listen(process.env.PORT || 8080);
